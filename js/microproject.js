f4fb98f95302c6edbcbb87c050083ec7d71dd626
dvanderschraaf <dvanderschraaf@sfsu.edu>
2026-03-19 13:47:42 -0700

    Commit

--- js/microproject.js
+++ js/microproject.js
@@ -1,31 +1,42 @@
-// --------------------
 // GLOBAL VARIABLES
-// --------------------
 
+//main variable controls everything = movement(drain), rest(recovery), collapsed(frozen)
 let energy = 100;
+
 let threshold = 30;
 
+//movement - square position
 let rectX;
 let rectY;
 
-
-// frames
+//frames
 let cooldown = 60; 
 
-
+//previous position
 let prevRectX;
 let prevRectY;
 
+//minimum energy to move
+let wakeThreshold = 10;
+
+//pause dureing collapse
+let isCollapsed = false;
+let collapseTimer = 0;
+
+//frozen state (3 seconds)
+let collapseDuration = 180;
+
+
 
 function setup() {
-  createCanvas(windowWidth * 0.8, windowHeight * 0.6);
-  rectX = width / 2;
-  rectY = height / 2;
-  rectMode(CENTER);
-  textAlign(CENTER);
+    createCanvas(windowWidth, windowHeight);
+    rectX = width / 2;
+    rectY = height / 2;
+    rectMode(CENTER);
+    textAlign(CENTER);
 
-  prevRectX = rectX;
-  prevRectY = rectY;
+    prevRectX = rectX;
+    prevRectY = rectY;
 
 }
 
@@ -39,11 +50,16 @@ function draw() {
     drawRectangle();
     drawUI();
   
-  }
+}
 
-  function updateMovement() {
+function updateMovement() {
 
+    if (isCollapsed) {
+      return;
+    }
+  
     if (energy > 70) {
+      //effort is movement speed - based on energy level
       let effort = map(energy, 70, 100, 0.02, 0.1);
       rectX = lerp(rectX, mouseX, effort);
       rectY = lerp(rectY, mouseY, effort);
@@ -51,29 +67,46 @@ function draw() {
     } else if (energy > 40) {
       rectX = lerp(rectX, mouseX, 0.02);
       rectY = lerp(rectY, mouseY, 0.02);
-  
-    } else if (energy > 10) {
+      //collapsed state (frozen)
+    } else if (energy > 0.01) {
       rectX = lerp(rectX, mouseX, 0.008);
       rectY = lerp(rectY, mouseY, 0.008);
   
-    } else if (energy > 0) {
-      rectX = lerp(rectX, mouseX, 0.002);
-      rectY = lerp(rectY, mouseY, 0.002);
-  
     } else {
-      energy = 0;
+      isCollapsed = true;
+      collapseTimer = 0; // start timer
     }
   
-  }
+}
 
-  function updateEnergy() {
-
+function updateEnergy() {
+    //measures amount moved - controls energy drain and recovery
     let movement = dist(rectX, rectY, prevRectX, prevRectY);
   
-    if (movement > 0.01) {
-      energy -= movement * 0.02;
+    if (isCollapsed) {
+  
+      collapseTimer++;
+  
+      //no recovery when frozen
+      if (collapseTimer < collapseDuration) {
+        //frozen
+      } else {
+        //recover
+        energy += 0.2;
+  
+        if (energy > wakeThreshold) {
+          isCollapsed = false;
+        }
+      }
+  
     } else {
-      energy += 0.15;
+  
+      if (movement > 0.05) {
+        energy -= movement * 0.04;
+      } else {
+        energy += 0.15;
+      }
+  
     }
   
     energy = constrain(energy, 0, 100);
@@ -81,9 +114,9 @@ function draw() {
     prevRectX = rectX;
     prevRectY = rectY;
   
-  }
+}
 
-  function drawEnvironment() {
+function drawEnvironment() {
 
     let r = map(energy, 100, 0, 0, 255);
     let g = map(energy, 100, 0, 200, 0);
@@ -91,55 +124,57 @@ function draw() {
   
     background(r, g, b);
   
-  }
+}
 
 function drawRectangle() {
+    //size makes the square shrink as energy drops
+    let size = map(energy, 0, 100, 20, 80);
+    //alspha makes the square fade as energy drops
+    let alpha = map(energy, 0, 100, 40, 255);
 
-  let size = map(energy, 0, 100, 20, 80);
-  let alpha = map(energy, 0, 100, 40, 255);
+    stroke(255);
+    strokeWeight(3.5);
+    noFill();
+    rect(rectX, rectY, size, size);
 
-  stroke(255);
-  strokeWeight(3.5);
-  noFill();
-  rect(rectX, rectY, size, size);
+    //energyHeight controls energy bar inside the square
+    let energyHeight = map(energy, 0, 100, 0, size);
 
-  let energyHeight = map(energy, 0, 100, 0, size);
+    noStroke();
+    fill(255, alpha);
 
-  noStroke();
-  fill(255, alpha);
-
-  let bottomY = rectY + size / 2;
-  rect(rectX, bottomY - energyHeight / 2, size, energyHeight);
+    //bottomY controls the energy bar to regenerate upwards
+    let bottomY = rectY + size / 2;
+    rect(rectX, bottomY - energyHeight / 2, size, energyHeight);
 
 }
 
 function drawUI() {
 
-  fill(0);
-  textSize(14);
-  text("Energy: " + floor(energy), width / 2, height - 40);
+    fill(0);
+    textSize(14);
+    text("Energy: " + floor(energy), width / 2, height - 40);
 
-  if (energy > 70) {
-    text("State: Active", width / 2, height - 20);
-
-  } else if (energy > 40) {
-    text("State: Tired", width / 2, height - 20);
-
-  } else if (energy > 10) {
-    text("State: Very Tired", width / 2, height - 20);
-
-  } else if (energy > 0.1) {
-    text("State: Critical", width / 2, height - 20);
-
-  } else {
-    text("State: Collapsed", width / 2, height - 20);
-  }
-
+    if (isCollapsed) {
+      text("State: Collapsed", width / 2, height - 20);
+  
+    } else if (energy > 70) {
+      text("State: Active", width / 2, height - 20);
+  
+    } else if (energy > 40) {
+      text("State: Tired", width / 2, height - 20);
+  
+    } else if (energy > 10) {
+      text("State: Very Tired", width / 2, height - 20);
+  
+    } else {
+      text("State: Critical", width / 2, height - 20);
+    }
 }
 
 
 
 
 function windowResized() {
-  resizeCanvas(windowWidth * 0.8, windowHeight * 0.6);
+    resizeCanvas(windowWidth, windowHeight);
 }   
